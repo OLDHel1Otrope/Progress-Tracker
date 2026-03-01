dbc731a0114bcff05e84a9016c0dd6a861917dcb
utkarsh <uppathak42@gmail.com>
2026-03-01 10:27:32 +0530

    use db url

--- lib/db.ts
+++ lib/db.ts
@@ -17,11 +17,10 @@ declare global {
 export const db =
   global.pgPool ||
   new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
 if (process.env.NODE_ENV !== "production") {
